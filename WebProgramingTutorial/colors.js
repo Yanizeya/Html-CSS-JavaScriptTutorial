2816a8afb355f83adcccf0b3334ba150bfb221e6
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-05-01 15:50:20 +0900

    Add files via upload

--- WebProgramingTutorial/colors.js
+++ WebProgramingTutorial/colors.js
@@ -1,21 +1,35 @@
-function nightDayHandle(self){
-    var target = document.querySelector('body');
-    var alist = document.querySelectorAll('a');
 
+
+var Links = {
+  
+  setColor : function(color){
+    // var alist = document.querySelectorAll('a');
+    // for(i=0; i<alist.length; i++){
+    //   alist[i].style.color = color;
+    // }
+    $('a').css('color', color); //jquery 라이브러리를 사용한 코드
+  }
+}
+var Body = {
+  setColor : function(color){
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor : function(color){
+    document.querySelector('body').style.backgroundcolor = color;
+  }
+}
+function nightDayHandle(self){
     if(self.value === 'night'){
-      target.style.backgroundColor = 'black';
-      target.style.color = 'white';
-      self.value = 'day';
-      for(var i=0; i<alist.length; i++){
-      alist[i].style.color = 'powderblue';
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      Links.setColor('powderblue');
+      self.value = 'day';      
       }
-    }
+
     else{
-      target.style.backgroundColor = 'white';
-      target.style.color = 'black';
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      Links.setColor('blue');
       self.value = 'night';
-      for(var i=0; i<alist.length; i++){
-      alist[i].style.color = 'blue';
-      }
     }
   }
