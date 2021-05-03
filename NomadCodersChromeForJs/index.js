3a0cdaa4370d805e8441f548f173d799cf4852fb
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-05-03 23:56:30 +0900

    Add files via upload

--- NomadCodersChromeForJs/index.js
+++ NomadCodersChromeForJs/index.js
@@ -1,18 +1,2 @@
-const IzyInfo = {
-    name : "Izuya",
-    age : 33,
-    gender : "male",
-    favMovie : [
-      {
-        name : "Eternal Sunshine",
-        starRating : "5/5"
-      },
-      {
-        name : "Leong",
-        starRating : "4.5/5"
-      }
-    ]
-  };
-  
-  console.log(IzyInfo.name);
-  console.log(IzyInfo.favMovie[0].name);
+  const title = document.getElementById("Muyaho");
+  title.innerHTML = "Hi! from Js";
