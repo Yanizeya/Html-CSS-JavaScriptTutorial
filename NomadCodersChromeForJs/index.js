3c1717ce13903c63b73c8a1a936c900bfefca061
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-05-04 22:00:25 +0900

    Add files via upload

--- NomadCodersChromeForJs/index.js
+++ NomadCodersChromeForJs/index.js
@@ -1,2 +1,20 @@
-  const title = document.getElementById("Muyaho");
-  title.innerHTML = "Hi! from Js";
+  const title = document.querySelector("#Muyaho");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(event){
+  const currentColor = title.style.color;
+  if(currentColor === BASE_COLOR)
+    title.style.color = OTHER_COLOR;
+  else {
+    title.style.color = BASE_COLOR;
+  }
+  console.log(currentColor);
+}
+
+function init(){
+  title.style.color = BASE_COLOR;
+  title.addEventListener("mouseenter", handleClick);
+}
+init();
