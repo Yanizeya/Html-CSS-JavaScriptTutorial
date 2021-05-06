4701a1e6baea048ddaae3555b3eeddaf391b1374
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-05-06 23:58:43 +0900

    Add files via upload

--- NomadCodersChromeForJs/todo.js
+++ NomadCodersChromeForJs/todo.js
@@ -2,15 +2,28 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
+  const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 function paintToDo(text){
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerText = '❌'
+  const newId = toDos.length+1;
   const span = document.createElement("span");
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id : newId
+  }
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
   function handleSubmit(event){
@@ -24,8 +37,14 @@ function paintToDo(text){
 const TODOS_LS = 'toDos';
 
 function loadToDos(){
-  const toDos = localStorage.getItem(TODOS_LS);
-  if(toDos !== null)  {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null)  {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo)){
+      
+    }
+      
+    });
   }
 }
   function init(){
